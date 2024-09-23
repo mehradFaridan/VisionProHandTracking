43afb8b788d203a5759ec207d760cf3c432a866c
mehradFaridan <faridan3838@gmail.com>
2024-09-22 22:47:03 -0600

    Initial Commit

---
+++ VisionProHandTrackingTests/VisionProHandTrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionProHandTrackingTests.swift
+//  VisionProHandTrackingTests
+//
+//  Created by Mehrad Faridan on 2024-09-22.
+//
+
+import Testing
+@testable import VisionProHandTracking
+
+struct VisionProHandTrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
